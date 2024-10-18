c4beba95503346e1cb8282ac631b76a672a1d1b5
zaid <159690637+hacxk@users.noreply.github.com>
2024-10-18 16:32:52 +0000

    feat: from scratche!

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: 'class',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,21 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          light: '#38bdf8', // sky-400
-          DEFAULT: '#0ea5e9', // sky-600
-          dark: '#0284c7', // sky-700
-        },
-        secondary: {
-          light: '#3b82f6', // blue-400
-          DEFAULT: '#2563eb', // blue-500
-          dark: '#1d4ed8', // blue-600
-        },
-        background: '#D3D3D3'
-      },
-      fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
-        inter: ['Inter', 'sans-serif'],
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
